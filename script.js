20189179c3eadeace2845e5c4bb3a8a2d34d2fb4
aminovsh <148354702+aminovsh@users.noreply.github.com>
2023-11-24 16:44:36 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+let students = [`radmir `, `shaxboz`, `jaxongir`, `xasan`, `daler`]
+
+let even_names = []
+let odd_names = []
+
+students.map((d , c) => {
+    if (ind % 2 === 0) {
+        even_names.push(std)
+    } else {
+        odd_names.push(std)
+    }
+}
+
+)
+
+console.log(even_names);
+console.log(odd_names);
